05cd0f80807e21903fbb75b66d7371d968d1a56b
MoeinFaghih <momeix.f78@gmail.com>
2024-12-08 15:53:36 +0300

    CarGallerySys Created

---
+++ CarRentalSystem/src/CarGallerySys.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class CarGallerySys {
+
+	private static ArrayList<Car> carGal= new ArrayList<>();
+	
+	public static void addCarGallery(ArrayList<Car> cars, String address, int id, String title) {
+		CarGallery gal = new CarGallery
+	}
+}
